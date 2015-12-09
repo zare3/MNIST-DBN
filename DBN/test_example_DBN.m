2a0b303047f0c6e5f7789fca4990b929fac909d6
zare3 <Home@MacBooks-iMac.local>
2015-12-09 04:00:49 +0200

    Bug in testing
    - There was a bug in the testing code that led to small error
    - I fixed the bug and had to change my network architecture — code now
    gets 1.1% error instead of 0.3% error

--- DBN/test_example_DBN.m
+++ DBN/test_example_DBN.m
@@ -1,4 +1,4 @@
-afunction test_example_DBN
+function test_example_DBN
 load mnist_uint8;
 
 train_x = double(train_x) / 255;
@@ -6,21 +6,27 @@ test_x  = double(test_x)  / 255;
 train_y = double(train_y);
 test_y  = double(test_y);
 
-
 rand('state',0)
-dbn.sizes = [256 256 256 256 256];
-opts.numepochs =   300;
+%train dbn
+dbn.sizes = [1024 1024 1024];
+opts.numepochs =   100;
 opts.batchsize = 100;
 opts.momentum  =   0.7;
-opts.alpha     =   0.5;
+opts.alpha     =   0.05;
 dbn = dbnsetup(dbn, train_x, opts);
 dbn = dbntrain(dbn, train_x, opts);
 
+%unfold dbn to nn
 nn = dbnunfoldtonn(dbn, 10);
 nn.activation_function = 'sigm';
 
+%train nn
 opts.numepochs =  300;
 opts.batchsize = 100;
 nn = nntrain(nn, train_x, train_y, opts);
 [er, bad] = nntest(nn, test_x, test_y);
 
+disp ('TEST ERROR IS :')
+disp (er)
+
+assert(er < 0.10, 'Too big error');
